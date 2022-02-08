ce25577fb958f313a1feb72ec789ff9b53740734
Kidam93 <52935960+Kidam93@users.noreply.github.com>
2022-02-08 20:51:45 +0100

    add routes

---
+++ node-js-jwt-auth/app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "kidam-secret-key"
+};
--- node-js-jwt-auth/server.js
+++ node-js-jwt-auth/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
+
 var corsOptions = {
     origin: "http://localhost:8081"
 };
@@ -9,6 +11,19 @@ app.use(cors(corsOptions));
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+
+//database
+const db = require("./app/models");
+const Role = db.role;
+
+//db.sequelize.sync();
+
+db.sequelize.sync({force: true}).then(() => {
+    console.log('Drop and Resync Db');
+    initial();
+});
+
+
 // simple route
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to bezkoder application." });
@@ -18,3 +33,34 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: "user"
+    });
+
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
+}
+
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+/*
+try {
+    await db.sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+*/
+
+
