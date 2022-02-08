847bf017019dc0728e2f568f9835b75b236f5d1b
Kidam93 <52935960+Kidam93@users.noreply.github.com>
2022-02-08 21:25:04 +0100

    patches relation models

--- node-js-jwt-auth/app/models/role.model.js
+++ node-js-jwt-auth/app/models/role.model.js
@@ -1,12 +1,10 @@
 module.exports = (sequelize, Sequelize) => {
-    return sequelize.define("users", {
-        username: {
-            type: Sequelize.STRING
+    return sequelize.define("roles", {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true
         },
-        email: {
-            type: Sequelize.STRING
-        },
-        password: {
+        name: {
             type: Sequelize.STRING
         }
     });
